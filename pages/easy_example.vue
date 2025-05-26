f373cbb440f0281a69f569b5e6669323986f595f
Swot <swotpp@163.com>
2025-05-26 23:07:04 +0800

    change @language to typescript

--- pages/easy_example.vue
+++ pages/easy_example.vue
@@ -1,8 +1,8 @@
-<!--@+leo-ver=5-thin-->
-<!--@+node:swot.20250522073435.6: * @file pages/easy_example.vue-->
-<!--@@language xml-->
+//@+leo-ver=5-thin
+//@+node:swot.20250522073435.6: * @file pages/easy_example.vue
+//@@language typescript
 <template lang="pug">
 div(class="text-3xl text-white bg-green-500 p-6 rounded shadow")
     | Tailwind 样式现在已生效
 </template>
-<!--@-leo-->
+//@-leo
