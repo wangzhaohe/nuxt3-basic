3978b4cbb91ab2119ebea3b2264873a86f27292b
Swot <swotpp@163.com>
2025-05-28 21:51:59 +0800

    add doc comment

--- app.vue
+++ app.vue
@@ -1,4 +1,14 @@
+//-@+leo-ver=5-thin
+//-@+node:swot.20250521194704.1: * @file app.vue
+//-@+doc
+//- [source,typescript]
+//- ----
+//-@@c
+//-@@language typescript
 <template lang="pug">
     NuxtLayout
         NuxtPage
 </template>
+//-@+doc
+//- ----
+//-@-leo
